1d94fcd78e0a330e418a7921820e7a52536354cf
sigchi-web <sigchi.dev@gmail.com>
2022-11-06 23:35:28 +0300

    init form login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {LoginFormModule} from "./login-form/login-form.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
